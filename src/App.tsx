9e60053d246bce5524460659e8b1a94a1eb6e8cc
MarcelLeal <marcel@pgentulhos.com.br>
2026-01-07 20:13:11 -0300

    developing

--- src/App.tsx
+++ src/App.tsx
@@ -243,7 +243,6 @@ export const App: React.FC = () => {
                     }}
                     autoFocus
                   />
-
                 </form>
               ) : (
                 <span
@@ -312,7 +311,6 @@ export const App: React.FC = () => {
               </a>
             </nav>
 
-            {/* this button should be disabled if there are no completed todos */}
             <button
               type="button"
               className="todoapp__clear-completed"
